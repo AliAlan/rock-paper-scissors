c0da3ca268eaf8640b069e4edbc0fbd0f24c23b2
ALI HAYDAR ALAN <alanalihaydar@gmail.com>
2023-02-16 19:56:30 +0300

    applying game results and making choices

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import { Header } from "./components";
+import { Game, GameResult, Header } from "./components";
+import { useState } from "react";
 function App() {
+  const choices = ["rock", "paper", "scissors"];
+  const [isOver, setIsOver] = useState(false);
+  const [data, setData] = useState([]);
+
+  const random = Math.floor(Math.random() * choices.length);
+  const clickHandler = (name, bg, img) => {
+    setData([...data, { name: name, bg: bg, img: img }]);
+    setIsOver((prevState) => (prevState = !prevState));
+  };
   return (
     <div>
       <Header />
+      {isOver ? (
+        <GameResult data={data} computerChoice={choices[random]} />
+      ) : (
+        <Game clickHandler={clickHandler} />
+      )}
     </div>
   );
 }
